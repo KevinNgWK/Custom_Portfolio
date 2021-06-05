269d6de4ec482a82e0d7269f2136d1a9b6134496
KevinNg <WNG052@e.ntu.edu.sg>
2021-06-05 20:27:00 +0800

    Added github button links to projects

--- src/components/ProjectsComponent.js
+++ src/components/ProjectsComponent.js
@@ -1,8 +1,9 @@
 import React, { useContext, useState, useEffect } from 'react';
 import Fade from 'react-reveal/Fade';
-import { Container, Row, Col, Accordion, Card } from 'react-bootstrap';
+import { Container, Row, Col, Accordion, Card, Button } from 'react-bootstrap';
 import NavigationBar from './NavBarComponent'
 import Title from './TitleComponent'
+import logo from '../data/images/GitHub-Mark-Light-32px.png'
 
 const ProjectCard = (props) => {
   return (
@@ -14,7 +15,7 @@ const ProjectCard = (props) => {
         <Accordion.Collapse eventKey={props.index.toString()}>
           <Card.Body>
             <Row>
-              <Col md={4}>
+              <Col md={{ span: 4, order: 1 }} xs={{ span: 12, order: 2 }}>
                 {props.project.info.map((information) => {
                   return (
                     <Card.Text>
@@ -22,8 +23,9 @@ const ProjectCard = (props) => {
                     </Card.Text>
                   );
                 })}
+                <Button href={props.project.repo}>Source Code <img src={logo} /></Button>
               </Col>
-              <Col md={{ span: 6, offset: 2}}>
+              <Col md={{ span: 6, offset: 2, order: 2}} xs={{ span: 12, order: 1 }}>
                 Temp placeholder for images/gifs
               </Col>
             </Row>
@@ -38,16 +40,16 @@ const Projects = (props) => {
 	return(
     <>
       <NavigationBar />
-      <Title title={"Projects"} />
-        <Container>
-          {props.projects.map((project, index) => {
-            return (
-              <Fade bottom duration={800} delay={index*400} distance="30px">
-                <ProjectCard project={project} index={index}/>
-              </Fade>
-            )
-          })}
-        </Container>
+      <Container>
+        <Title title={"Projects"} />
+        {props.projects.map((project, index) => {
+          return (
+            <Fade bottom duration={800} delay={index*400} distance="30px">
+              <ProjectCard project={project} index={index}/>
+            </Fade>
+          )
+        })}
+      </Container>
     </>
 	);
 }
